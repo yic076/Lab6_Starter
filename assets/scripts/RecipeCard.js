f3fbd32e34d0824a9da8ccbe7bfacc2e122bfd14
yic076 <yic076@ucsd.edu>
2022-11-07 22:55:03 -0800

    lab6

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -138,7 +138,7 @@ class RecipeCard extends HTMLElement {
       <p class="organization">${data["organization"]}</p>
       <div class="rating">
         <span>${data["rating"]}</span>
-        <img src="../assets/images/icons/${data["rating"]}-star.svg" alt="5 stars">
+        <img src="assets/images/icons/${data["rating"]}-star.svg" alt="5 stars">
         <span>(${data["numRatings"]})</span>
       </div>
       <time>${data["lengthTime"]}</time>
